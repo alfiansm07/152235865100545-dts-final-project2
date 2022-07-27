07d9e8f8ef1afc5f7f26e2d84c6c1c647dcf88d6
alfiansm07 <alfiansm07@gmail.com>
2022-07-27 15:56:22 +0700

    Update BannerTrending.jsx

--- src/components/BannerTrending.jsx
+++ src/components/BannerTrending.jsx
@@ -1,12 +1,32 @@
 import useAxios from "../hooks/useAxios";
 
 import React from "react";
+import { Container } from "@mui/system";
+import { Alert, AlertTitle } from "@mui/material";
 
 const BannerTrending = () => {
-  const [{ data, loading, error }, refetch] = useAxios(
-    "/api/detail/2021/01/28/balan-wonderworld-preview"
-  );
-  console.log("dataku", data);
+  const [{ data, loading, error }, refetch] = useAxios("/api/games?page=1");
+  console.log(data);
+  const { author, desc, key, tag, thumb, time, title } = data[0];
+  console.log("test ", title);
+  if (loading) {
+    return <></>;
+  }
+
+  if (error) {
+    return (
+      <Container
+        sx={{
+          paddingY: 5,
+        }}
+      >
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error.message}
+        </Alert>
+      </Container>
+    );
+  }
 
   return <></>;
 };
