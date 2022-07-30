dec39ee27cca754b66375bf810620c3d246ff2a6
alfiansm07 <alfiansm07@gmail.com>
2022-07-30 20:52:19 +0700

    Update SearchPage.jsx

--- src/containers/SearchPage.jsx
+++ src/containers/SearchPage.jsx
@@ -1,10 +1,37 @@
+import { Alert, AlertTitle } from "@mui/material";
+import { Container } from "@mui/system";
 import React from "react";
 import { useParams } from "react-router-dom";
+import Loading from "../components/Loading";
+import useAxios from "../hooks/useAxios";
 
 const SearchPage = () => {
   let { query } = useParams();
-  console.log("ini query", query);
-  return <div>SearchPage</div>;
+
+  const [{ data, loading, error }, refetch] = useAxios(
+    `/api/search?search=${query}`
+  );
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return (
+      <Container
+        sx={{
+          paddingY: 5,
+        }}
+      >
+        <Alert severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {error.message}
+        </Alert>
+      </Container>
+    );
+  }
+
+  console.log("ini data dari search page", data);
+  return <div>{query}</div>;
 };
 
 export default SearchPage;
